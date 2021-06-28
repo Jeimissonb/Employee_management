ced6cfeba6f38321e08ddadc0943d44d121d97b9
Jeimissonb <jeimissonb@hotmail.com>
2021-06-28 14:46:25 -0300

    Created add employee in controller

--- src/main/java/com/example/backend/controller/EmployeeController.java
+++ src/main/java/com/example/backend/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.example.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.model.Employee;
 import com.example.backend.repository.EmployeeRepository;
 
+@CrossOrigin (origins = "http://localhost:3000/")
 @RestController
 @RequestMapping(value = "/api/v1")
 public class EmployeeController {
@@ -21,6 +25,12 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees (){
 		return employeeRepository.findAll();
+	}
+	
+	//create add employee
+	@PostMapping("/employees")
+	public Employee createEmployee (@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 	
 }
